b82aa03764339f361eb622665c7a8a7eab50dd01
ryochan1101 <ryo1101@mercari.com>
2016-05-11 16:20:20 +0900

    サンプルを整理

--- RxDataSourceSample/RxDataSourceSample/EdittingTableviewViewController.swift
+++ RxDataSourceSample/RxDataSourceSample/EdittingTableviewViewController.swift
@@ -24,44 +24,31 @@ class EdittingTableviewViewController: UIViewController {
         let dataSource = RxTableViewSectionedAnimatedDataSource<NumberSection>()
         let sections: [NumberSection] = [
             NumberSection(header: "Section 1", numbers: [], updated: NSDate()),
-            NumberSection(header: "Section 2", numbers: [], updated: NSDate()),
-            NumberSection(header: "Section 3", numbers: [], updated: NSDate())
         ]
         
         let initialState = SectionedTableViewState(sections: sections)
-        let addCommand = addButton.rx_tap
-            .scan(0) { x, _ in x+1 }
-            .map { number -> TableViewEdittingCommand in
-                let randSection = Int(arc4random_uniform(UInt32(sections.count)))
-                let item = IntItem(number: number, date: NSDate())
-                return TableViewEdittingCommand.AppendItem(item: item, section: randSection)
+        let addCommand = addButton.rx_tap.asDriver()
+            .map { _ -> TableViewEdittingCommand in
+                let item = IntItem(number: 1, date: NSDate())
+                return TableViewEdittingCommand.AppendItem(item: item, section: 0)
             }
         
-        let deleteCommand = tableView.rx_itemDeleted
+        let deleteCommand = tableView.rx_itemDeleted.asDriver()
             .map { indexPath -> TableViewEdittingCommand in
                 return TableViewEdittingCommand.DeleteItem(indexPath)
             }
         
-        Observable
+        let updatedState = Driver
             .of(addCommand, deleteCommand)
             .merge()
             .scan(initialState) { return $0.executeCommand($1) }
             .startWith(initialState)
             .map { $0.sections }
-            .shareReplay(1)
-            .bindTo(tableView.rx_itemsAnimatedWithDataSource(dataSource))
+        
+        updatedState
+            .drive(tableView.rx_itemsAnimatedWithDataSource(dataSource))
             .addDisposableTo(disposeBag)
         
-        skinTableViewDataSource(dataSource)
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    private func skinTableViewDataSource(dataSource: RxTableViewSectionedAnimatedDataSource<NumberSection>) {
         dataSource.animationConfiguration = AnimationConfiguration(insertAnimation: .Top, reloadAnimation: .Fade, deleteAnimation: .Left)
         
         dataSource.configureCell = { (dataSource, tableView, indexPath, item) in
@@ -80,39 +67,9 @@ class EdittingTableviewViewController: UIViewController {
             return true
         }
     }
-}
 
-enum TableViewEdittingCommand {
-    case AppendItem(item: IntItem, section: Int)
-    case DeleteItem(NSIndexPath)
-}
-
-struct SectionedTableViewState {
-    private var sections: [NumberSection]
-    
-    init(sections: [NumberSection]) {
-        self.sections = sections
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-    
-    func executeCommand(command: TableViewEdittingCommand) -> SectionedTableViewState {
-        switch command {
-        case .AppendItem(let appendEvent):
-            var sections = self.sections
-            let items = sections[appendEvent.section].items + appendEvent.item
-            sections[appendEvent.section] = NumberSection(original: sections[appendEvent.section], items: items)
-            return SectionedTableViewState(sections: sections)
-        case .DeleteItem(let indexPath):
-            var sections = self.sections
-            var items = sections[indexPath.section].items
-            items.removeAtIndex(indexPath.row)
-            sections[indexPath.section] = NumberSection(original: sections[indexPath.section], items: items)
-            return SectionedTableViewState(sections: sections)
-        }
-    }
-}
-
-func + <T>(lhs: [T], rhs: T) -> [T] {
-    var copy = lhs
-    copy.append(rhs)
-    return copy
 }
---
+++ RxDataSourceSample/RxDataSourceSample/SectionedTableViewState.swift
@@ -0,0 +1,46 @@
+//
+//  SectionedTableViewState.swift
+//  RxDataSourceSample
+//
+//  Created by mercari on 5/11/16.
+//  Copyright © 2016 Ryo. All rights reserved.
+//
+
+import RxDataSources
+import RxSwift
+import RxCocoa
+
+struct SectionedTableViewState {
+    var sections: [NumberSection]
+    
+    init(sections: [NumberSection]) {
+        self.sections = sections
+    }
+    
+    func executeCommand(command: TableViewEdittingCommand) -> SectionedTableViewState {
+        switch command {
+        case .AppendItem(let appendEvent):
+            var sections = self.sections
+            let items = sections[appendEvent.section].items + appendEvent.item
+            sections[appendEvent.section] = NumberSection(original: sections[appendEvent.section], items: items)
+            return SectionedTableViewState(sections: sections)
+        case .DeleteItem(let indexPath):
+            var sections = self.sections
+            var items = sections[indexPath.section].items
+            items.removeAtIndex(indexPath.row)
+            sections[indexPath.section] = NumberSection(original: sections[indexPath.section], items: items)
+            return SectionedTableViewState(sections: sections)
+        }
+    }
+}
+
+enum TableViewEdittingCommand {
+    case AppendItem(item: IntItem, section: Int)
+    case DeleteItem(NSIndexPath)
+}
+
+func + <T>(lhs: [T], rhs: T) -> [T] {
+    var copy = lhs
+    copy.append(rhs)
+    return copy
+}
